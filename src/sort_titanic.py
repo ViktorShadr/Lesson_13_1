02a5dd22ceb9200440aca17978c88800ff47c2c3
Viktor Shadrin <v.vshadrin@gmail.com>
2025-07-17 22:12:20 +0300

    доделал функцию сортировки пассажиров по полу и среднему возрасту

---
+++ src/sort_titanic.py
@@ -0,0 +1,23 @@
+import json
+
+import pandas as pd
+
+
+sort_passengers_by_avg_grade = pd.read_csv('..//data/titanic.csv')
+
+new_sort = sort_passengers_by_avg_grade.groupby('Sex')['Age'].mean()
+
+new_sort.to_json('..//data/sort_passengers.json', indent=4)
+
+def avg_age_by_gender(df):
+    avg_age_male = df[df['Sex'] == 'male']['Age'].mean()
+    avg_age_female = df[df['Sex'] == 'female']['Age'].mean()
+    result_dict = {'Мужчины': avg_age_male, 'Женщины': avg_age_female}
+    return json.dumps(result_dict)
+
+def avg_age_by_gender_simple(df):
+    return df.groupby('Sex')['Age'].mean().round(2).to_json()
+
+
+print(json.loads(avg_age_by_gender(sort_passengers_by_avg_grade)))
+print(json.loads(avg_age_by_gender_simple(sort_passengers_by_avg_grade)))
